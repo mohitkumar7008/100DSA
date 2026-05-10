837ff50030bff5ca7b43b88afd50f640165915b2
mohitkumar7008 <mohitkamboj7008@gmail.com>
2026-05-10 23:17:04 +0530

    sum of longest path

--- 100.cpp
+++ 100.cpp
@@ -344,5 +344,51 @@ class Solution {
         return ans;
     }
 };
+/*
+class Node {
+  public:
+    int data;
+    Node *left;
+    Node *right;
 
->>>>>>> 80f3743 (vertical order traversal)
+    Node(int x) {
+        data = x;
+        left = NULL;
+        right = NULL;
+    }
+}; */
+
+class Solution {
+  public:
+  void solve(Node* root,int sum,int &maxSum,int len,int &maxLen)
+  {
+      if(root==NULL)
+      {
+           if(len>maxLen)
+          {
+              maxLen=len;
+              maxSum=sum;
+          }
+          else if (len==maxLen)
+          {
+              maxSum=max(sum,maxSum);
+          }
+         
+          return;
+      }
+      sum=sum+root->data;
+      solve(root->left,sum,maxSum,len+1,maxLen);
+      solve(root->right,sum,maxSum,len+1,maxLen);
+  }
+    int sumOfLongRootToLeafPath(Node *root) {
+        // code here
+        int sum=0;
+        int len=0;
+        int maxSum=0;
+        int maxLen=0;
+        solve(root,sum,maxSum,len,maxLen);
+        
+        return maxSum;
+       
+    }
+};
