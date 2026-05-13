f4f1af9976bbabfdeee985d5f26951e0baf527b2
mohitkumar7008 <mohitkamboj7008@gmail.com>
2026-05-13 23:00:21 +0530

    solved 1s surrounded by 0s

--- 100.cpp
+++ 100.cpp
@@ -419,3 +419,52 @@ class Solution {
         }
     }
 };
+// User function Template for C++
+
+class Solution {
+  public:
+    void dfs(vector<vector<int>>& grid, int r, int c) {
+        int m = grid.size();
+        int n = grid[0].size();
+        
+        if(r < 0 || c < 0 || r >= m || c >= n || grid[r][c] == 0)
+            return;
+        
+        grid[r][c] = 0;
+        
+        dfs(grid, r+1, c);
+        dfs(grid, r-1, c);
+        dfs(grid, r, c+1);
+        dfs(grid, r, c-1);
+    }
+    int numberOfEnclaves(vector<vector<int>> &grid) {
+        // Code here
+        int m=grid.size();
+        int n=grid[0].size();
+        for(int i=0;i<m;i++){
+            if(grid[i][0]==1){
+                dfs(grid,i,0);
+            }
+            if(grid[i][n-1]==1){
+                dfs(grid,i,n-1);
+            }
+        }
+        for(int i=0;i<n;i++){
+            if(grid[0][i]==1){
+                dfs(grid,0,i);
+            }
+            if(grid[m-1][i]==1){
+                dfs(grid,m-1,i);
+            }
+        }
+        int count = 0;
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(grid[i][j] == 1)
+                    count++;
+            }
+        }
+        
+        return count;
+    }
+};
