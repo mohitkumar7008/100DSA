d0ae5c16cacba69d7cf44f8cb4148a136d9cf3cf
mohitkumar7008 <mohitkamboj7008@gmail.com>
2026-05-20 23:06:42 +0530

    triplet sum in array

--- 100.cpp
+++ 100.cpp
@@ -699,3 +699,37 @@ public:
         return ans;
     }
 };
+class Solution {
+  public:
+    bool hasTripletSum(vector<int> &arr, int target) {
+        // Code Here
+        sort(arr.begin(),arr.end());
+        for(int i=0;i<arr.size();i++)
+        {
+            // if(i>0&&arr[i]==arr[i-1])continue;
+            int j=i+1;
+            int k=arr.size()-1;
+            while(j<k)
+            {
+                int sum=arr[i]+arr[j]+arr[k];
+                if(sum>target)
+                {
+                    k--;
+                }
+                else if(sum<target)
+                {
+                    j++;
+                }
+                else
+                {
+                    return true;
+                }
+                // while(j<k&&arr[j-1]==arr[j])
+                // {
+                //     j++;
+                // }
+            }
+        }
+        return false;
+    }
+};
