91a6ac2ff71ea7fa1fd3ff5635f9f31b1ff4b41d
mohitkumar7008 <mohitkamboj7008@gmail.com>
2026-05-23 22:41:01 +0530

    stock buy sell best time

--- 100.cpp
+++ 100.cpp
@@ -773,4 +773,59 @@ public:
         return maxProfit;
     }
 };
-
+class Solution {
+  public:
+  int merge(vector<int>&arr,int st,int mid,int end)
+  {
+      int i=st;
+      int j=mid+1;
+      vector<int>temp;
+      int count=0;
+      while(i<=mid&&j<=end)
+      {
+          if(arr[i]<=arr[j])
+          {
+              temp.push_back(arr[i]);
+              i++;
+          }
+          else
+          {
+              temp.push_back(arr[j]);
+              j++;
+              count=count+mid+1-i;
+          }
+      }
+      while(i<=mid)
+      {
+          temp.push_back(arr[i]); i++;
+          
+      }
+      while(j<=end)
+      {
+          temp.push_back(arr[j]);
+          j++;
+      }
+      for(int i=0;i<temp.size();i++)
+      {
+          arr[i+st]=temp[i];
+      }
+      return count;
+  }
+  int mergesort(vector<int>&arr,int st,int end)
+  {
+      if(st<end)
+      {
+          int mid=st+(end-st)/2;
+          int leftinversion=mergesort(arr,st,mid);
+          int rightinversion=mergesort(arr,mid+1,end);;
+          int countinversion=merge(arr,st,mid,end);
+          return countinversion+leftinversion+rightinversion;
+          
+      }
+      return 0;
+  }
+    int inversionCount(vector<int> &arr) {
+        // Code Here
+        return mergesort(arr,0,arr.size()-1);
+    }
+};mohit
