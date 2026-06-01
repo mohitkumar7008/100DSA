4634371ead4377586af178ad1c8bc53483b6842f
mohitkumar7008 <mohitkamboj7008@gmail.com>
2026-06-02 01:40:31 +0530

    stm

--- 100.cpp
+++ 100.cpp
@@ -934,3 +934,47 @@ class Solution {
         reverse(arr.begin()+e,arr.end());
     }
 };
+
+class Solution {
+  public:
+    vector<int> spirallyTraverse(vector<vector<int>> &mat) {
+        // code here
+        vector<int>ans;
+        int top=0;
+        int bottom=mat.size()-1;
+        int left=0;
+        int right=mat[0].size()-1;
+        while(top<=bottom && left<=right)
+        {
+            for(int i=left;i<=right;i++)
+            {
+                ans.push_back(mat[top][i]);
+            }
+            top++;
+            for(int i=top;i<=bottom;i++)
+            {
+                ans.push_back(mat[i][right]);
+            }
+            right--;
+            if(top<=bottom)
+            for(int i=right;i>=left;i--)
+            {
+                ans.push_back(mat[bottom][i]);
+            }
+            bottom--;
+            if(left<=right)
+            for(int i=bottom;i>=top;i-- )
+            {
+                ans.push_back(mat[i][left]);
+            }
+            left++;
+            
+        }
+        return ans;
+        // for(int i=0;i<ans.size();i++)
+        // {
+        //     cout<<ans[i]<<endl;
+        // }
+        // return {};
+    }
+};hhhh
